75a5b62c9749afe72121c26a1eab65147809b516
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-01-25 12:24:52 -0500

    "NavBar y conf listas"

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,8 @@ import NavBar from "./NavBar"
 
 const Home = () => { 
   return(
-    <main>
-      <h1 className="bg-pink-500 w-screen text-center text-5xl text-white font-Principal">Home BEESMRT</h1>
+    <main className="backgroundGradient1 w-screen h-screen">
+      <h1 className="backgroundGradient2 w-screen text-center text-5xl text-white font-Principal">Home BEESMRT</h1>
       <NavBar/>
     </main>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ export default {
         "Principal":['"Bebas Neue"', 'sans-serif'],
         "Secundaria":['"Roboto"', 'sans-serif']
       },
+      colors:{
+        "Yellow1": "#FFC924",
+        "Yellow2":"#F8FB47",
+        "Yellow3": "#F9CB67",
+        "Pink1": "#FB43C1",
+        "Pink2": "#FA7A9D",
+        "Pink3": "#FA9D85"
+      },
     },
   },
   plugins: [],
