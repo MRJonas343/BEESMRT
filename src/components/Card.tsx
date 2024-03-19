62baa39fc37a9cab21ef7144b6dd7b4cd05a2fe1
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-18 21:16:24 -0500

    home 3.0

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,8 +6,8 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ imageSrc, title, text }) => {
   return (
-    <div className="bg-white/70 w-[43vw] flex flex-col items-center overflow-hidden p-5 rounded-2xl ease-in-out duration-200 hover:scale-105 hover:bg-yellow-400 cursor-pointer 2xl:w-[22vw] 2xl:h-[50vh]">
-      <img src={imageSrc} alt={title} className="w-3/4 md:w-36 xl:max-w-48"/>
+    <div className="bg-white/70 w-[43vw] flex flex-col items-center overflow-hidden p-5 rounded-2xl ease-in-out duration-200 hover:scale-105 hover:bg-yellow-400 cursor-pointer lg:w-[22vw] lg:h-[50vh]">
+      <img src={imageSrc} alt={title} className="w-3/4 max-w-40  lg:max-w-48"/>
       <h2 className="font-Principal text-2xl py-3 text-center text-3d text-white md:text-4xl">{title}</h2>
       <p className=" text-clip overflow-y-auto font-Principal text-l font-thin md:text-2xl">{text}</p>
     </div>
