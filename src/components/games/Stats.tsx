e8121b41a85a73907ba366212843ccf838311a14
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-19 07:19:26 -0500

    Fixing Memory Game

--- src/components/games/Stats.tsx
+++ src/components/games/Stats.tsx
@@ -15,28 +15,28 @@ const Stats : React.FC<StatProps> = ({ isPlayer1Active, player1Points, player2Po
 
     useEffect(() => {
         if (isPlayer1Active) {
-            setPlayer1Class("text-xl rounded-sm p-2 mb-5 outline outline-blue-600");
-            setPlayer2Class("text-xl rounded-sm p-2 mb-5");
+            setPlayer1Class("text-lg rounded-sm p-2 mb-2 outline outline-blue-600")
+            setPlayer2Class("text-lg rounded-sm p-2 mb-2")
         } else {
-            setPlayer1Class("text-xl rounded-sm p-2 mb-5");
-            setPlayer2Class("text-xl rounded-sm p-2 mb-5 outline outline-blue-600");
+            setPlayer1Class("text-lg rounded-sm p-2 mb-2")
+            setPlayer2Class("text-lg rounded-sm p-2 mb-2 outline outline-blue-600")
         }
-    }, [isPlayer1Active]);
+    }, [isPlayer1Active])
 
     
 
     return (
     <section className="flex justify-evenly xl:flex-col xl:h-[80vh]">
-        <div className="bg-white/80 px-8 py-6 font-Principal2 text-center rounded-xl md:w-56">
-            <h2 className="text-2xl mb-5">TURN: </h2>
+        <div className="bg-white/80 px-8 py-4 font-Principal2 text-center rounded-xl md:w-56">
+            <h2 className="text-xl mb-2">TURN: </h2>
             <p className={Player1Class}>PLAYER 1</p>
             <p className={Player2Class}>PlAYER 2</p>
         </div>
-        <div className="bg-white/80 px-8 py-6 font-Principal2 text-center rounded-xl md:w-56">
-            <h2 className="text-2xl mb-5">SCORE</h2>
-            <p className="text-xl p-2 mb-2">PlAYER 1: {player1Points}</p>
-            <p className="text-xl p-2 mb-2">PlAYER 2: {player2Points}</p>
-            <button className="text-xl bg-red-600 rounded-md p-3 w-full text-white" onClick={playAgain}>RESET</button>
+        <div className="bg-white/80 px-8 py-4 font-Principal2 text-center rounded-xl md:w-56">
+            <h2 className="text-xl mb-2">SCORE</h2>
+            <p className="text-lg p-2 mb-2">PlAYER 1: {player1Points}</p>
+            <p className="text-lg p-2 mb-2">PlAYER 2: {player2Points}</p>
+            <button className="text-lg bg-red-600 rounded-md p-2 w-full text-white" onClick={playAgain}>RESET</button>
         </div>
     </section>
     )
