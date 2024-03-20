cb7ad426b13333c946981f7d8e2ad515fee924c4
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-20 07:02:02 -0500

    home 2.5

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,8 +12,8 @@ const Home: React.FC = () => {
     <main className="w-screen h-screen bg-Gradient1 overflow-x-hidden">
       <NavBar/>
       
-      <h1 className="text-4xl text-center pb-3 text-white text-3d2 font-Principal2 from-neutral-400 md:text-4xl md:py-5 lg:text-6xl xl:py-14">BEESMRT GAMES</h1>
-      <section className="grid grid-cols-2 justify-items-center gap-4 h-[83vh] lg:flex lg:justify-center lg:items-center lg:gap-9 lg:h-[70vh]">
+      <h1 className="text-4xl text-center pb-3 text-white text-3d2 font-Principal2 from-neutral-400 md:text-4xl md:py-5 lg:text-6xl xl:py-14  xl:py-8">BEESMRT GAMES</h1>
+      <section className="bg-red-500 grid grid-cols-2 justify-items-center gap-4 h-[83vh] lg:flex lg:justify-center lg:items-center lg:gap-9 lg:h-[70vh]">
 
         <Link to="/games/TableGame" className="flex">
           <Card
