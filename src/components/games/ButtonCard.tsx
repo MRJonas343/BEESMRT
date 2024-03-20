d5eca18bfc21f0e3d1fda9b998000767c8179390
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-20 12:45:56 -0500

    memory 2.4

--- src/components/games/ButtonCard.tsx
+++ src/components/games/ButtonCard.tsx
@@ -9,12 +9,12 @@ interface ButtonProps {
 const ButtonCard: React.FC<ButtonProps> = ({ card, flipped, chooseCard }) => {
   const cardHandleClick = () => chooseCard(card)
 
-  let classCard = `card ${flipped ? 'matched' : ''} p-2 w-20 h-14 bg-gray-100 flex relative justify-center items-center cursor-pointer  md:w-24 md:h-20 xl:w-36 xl:h-32`
+  let classCard = `card ${flipped ? 'matched' : ''} w-20 h-14 bg-gray-100 flex relative justify-center items-center cursor-pointer  md:w-24 md:h-20 xl:w-36 xl:h-32`
 
   return (
     <div className={classCard} onClick={cardHandleClick}>
-      <img className="cardImg min-w-11 max-w-14 md:w-20 md:max-w-3xl absolute" alt="" src={card.src} />
-      <img className="cardBack min-w-11 max-w-14 md:w-20 md:max-w-3xl absolute hover:scale-110 duration-200 ease-in-out" src={BackCardImg} />
+      <img className="cardImg w-12 md:w-20  absolute xl:w-24 p-1 " alt="" src={card.src} />
+      <img className="cardBack w-12 md:w-20 xl:w-24 p-1 absolute hover:scale-110 duration-200 ease-in-out" src={BackCardImg} />
     </div>
   )
 }
