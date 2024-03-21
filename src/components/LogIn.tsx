ca5f6b56be53d47cbe8fad6c3744caa7814b7763
Jonas <144829167+MRJonas343@users.noreply.github.com>
2024-03-21 08:30:08 -0500

    nuevo log in

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,18 +1,28 @@
 import NavBar from "./NavBar"
 
 const LogIn = () => {
-
-    
-    
     return (
         <main className="w-screen h-screen bg-Gradient1">
             <NavBar/>
-            <h1 className="text-5xl text-center">Log In</h1>
-            <section className="flex flex-col items-center">
-                <input className="border border-gray-300 rounded-md px-3 py-2 mt-2" placeholder="Email" type="text"></input>
-                <input className="border border-gray-300 rounded-md px-3 py-2 mt-2" placeholder="Password" type="password"></input>
-                <div className="fb-login-button" data-width="200" data-size="" data-button-type="" data-layout="" data-auto-logout-link="false" data-use-continue-as="false"></div>
-            </section>
+        <div className="flex justify-center">
+            <div className="bg-white rounded-md p-7 mt-8 text-center w-96 shadow-2xl">
+                <span className="font-Principal text-3xl">LOGIN</span> <br />
+                <div className="text-left">
+                    Email: <br />
+                    <input type="text" className="bg-gray-100 border border-black rounded-md w-full h-9 p-2"/>
+                </div>
+                <div className="text-left pb-4">
+                    Password <br />
+                    <input type="password" className="bg-gray-100 border border-black rounded-md w-full h-9 p-2"/>
+                </div>
+                <div>
+                    <button className="border border-black font-Principal text-xl p-2 w-28 transition duration-300 hover:bg-yellow-300">LOGIN</button>
+                </div>
+                <div className="Footer-Login">
+                    Don't you have a account yet? <br /> <a href="" className="text-indigo-700 font-semibold">Get an Account</a>
+                </div>
+            </div>
+        </div>
         </main>
     )
 }
